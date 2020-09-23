5179bfc723c1c6ac57c26b6843db8e5b61db88e2
IvanCaballero1204 <gabriel.gradilla@alumnos.udg.mx>
2020-09-23 09:42:56 -0500

    Segundo Commit de la rama master. Se introdujo toda la operacion en una función main()

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
-print("Hola mundo")
-nombre = input("¿Cual es tu nombre? ")
-print("Hola {}".format(nombre))
-edad = int(input("¿Cuántos años tienes? "))
-print("{} tienes {} años y en un año tendrás {} años".format(nombre, edad, edad + 1))
+def main():
+    print("Hola mundo")
+    nombre = input("¿Cual es tu nombre? ")
+    print("Hola {}".format(nombre))
+    edad = int(input("¿Cuántos años tienes? "))
+    print("{} tienes {} años y en un año tendrás {} años".format(nombre, edad, edad + 1))
+
+main()
