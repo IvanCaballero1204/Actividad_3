2c66efede81b97e59c196837690641e2a9076c9b
IvanCaballero1204 <gabriel.gradilla@alumnos.udg.mx>
2020-09-23 09:36:13 -0500

    Primer commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+print("Hola mundo")
+nombre = input("¿Cual es tu nombre? ")
+print("Hola {}".format(nombre))
+edad = int(input("¿Cuántos años tienes? "))
+print("{} tienes {} años y en un año tendrás {} años".format(nombre, edad, edad + 1))
