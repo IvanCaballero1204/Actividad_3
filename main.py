5aab2345403475926a1b32400274c05546116c39
IvanCaballero1204 <gabriel.gradilla@alumnos.udg.mx>
2020-09-23 10:01:24 -0500

    Primer commit prueba

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+<<<<<<< HEAD
 def main():
     print("Hola mundo")
     nombre = input("¿Cual es tu nombre? ")
@@ -9,3 +10,13 @@ def main():
 
 print("Bienvenido a mi programa")
 main()
+=======
+def main():
+    print("Hola mundo")
+    nombre = input("¿Cual es tu nombre? ")
+    print("Hola {}".format(nombre))
+    edad = int(input("¿Cuántos años tienes? "))
+    print("{} tienes {} años y en un año tendrás {} años".format(nombre, edad, edad + 1))
+
+main()
+>>>>>>> parent of 5179bfc... Segundo Commit de la rama master. Se introdujo toda la operacion en una función main()
